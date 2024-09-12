fb3152a04f21d779f859ea33050a19e30d488f70
SAGAR T <ssagu6661@gmail.com>
2024-09-12 09:37:08 -0400

    Add files via upload

---
+++ basic_program2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+int x=10,y;
+y=++x;                          /*prefix*/
+printf("Valu of x is :   %d\n",x);
+printf("Value of y is :  %d\n",y);
+printf("\n");
+
+int a=10,b;
+b=a++;                          /*POSTfix*/
+printf("valu of a is : %d\n",a);
+printf("value of b is : %d\n",b);
+}
+
+
+
+                                
